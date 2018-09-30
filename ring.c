6d407cd43adee8ce53b16e8cd94aee2a5b6eff6b
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-09-29 21:38:24 -0400

    user inout

--- ring.c
+++ ring.c
@@ -60,11 +60,10 @@ int main(int argc, char* argv[])
           //read(fd[0], buffer, sizeof(buffer));
           //printf("Received string: %s at %d\n", buffer, child);
           printf("Pipe between parent %d and child %d\n", getpid(), child);
+          execvp("ps","-l");
       }
       sleep(5);
     }
-    printf("process");
-    //pause();
     // test commit stuff
 		// pid = fork();
 		// if(pid < 0){
