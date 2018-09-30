539fdeb44eff8abaad9751574eed9ea7f3c8c8e0
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-09-30 16:16:02 -0400

    info

--- test.c
+++ test.c
@@ -24,65 +24,65 @@ int main(int argc, char* argv[])
     char buffer[80];
     char* output = "STRING OUTPUT";
     printf("Parent pid: %d\n\n", getpid());
-      pipe(fd);
-      pid = fork();
-      if(pid < 0) {
-          printf("Error");
-          exit(1);
-      } else if (pid == 0) { // child
-          printf("Child (%d): %d Parent: %d\n", 1, getpid(), getppid());
 
-          pipe(fd);
-          close(fd[1]);
-          printf("Reading from pipe??\n");
-          read(fd[0], buffer, sizeof(buffer));
-          printf("Received string: %s at %d\n", buffer, getpid());
+    pid = fork();
+    if(pid < 0) {
+        printf("Error");
+        exit(1);
+    } else if (pid == 0) { // child
+        printf("Child (%d): %d Parent: %d\n", 1, getpid(), getppid());
 
-          bpid = fork();
-          if(bpid < 0) {
-              printf("Error");
-              exit(1);
-          } else if (bpid == 0) { // child
-              printf("Child (%d): %d Parent: %d\n", 2, getpid(), getppid());
+        pipe(fd);
+        close(fd[1]);
+        printf("Reading from pipe??\n");
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Received string: %s at %d\n", buffer, getpid());
 
-              pipe(fd);
-              close(fd[1]);
-              printf("Reading from pipe??\n");
-              read(fd[0], buffer, sizeof(buffer));
-              printf("Received string: %s at %d\n", buffer, getpid());
-              cpid = fork();
-              if(cpid < 0) {
-                  printf("Error");
-                  exit(1);
-              } else if (cpid == 0) { // child
-                  printf("Child (%d): %d Parent: %d\n", 3, getpid(), getppid());
-                  close(fd[1]);
-                  printf("Reading from pipe??\n");
-                  read(fd[0], buffer, sizeof(buffer));
-                  printf("Received string: %s at %d\n", buffer, getpid());
-                  exit(0);
-              } else  {
-                close(fd[0]);
-                printf("writing to pipe??\n");
-                write(fd[1], output, (strlen(output)+1));
-                wait(NULL);
-              }
+        bpid = fork();
+        if(bpid < 0) {
+            printf("Error");
+            exit(1);
+        } else if (bpid == 0) { // child
+            printf("Child (%d): %d Parent: %d\n", 2, getpid(), getppid());
 
-              exit(0);
-          } else  {
-            close(fd[0]);
-            printf("writing to pipe??\n");
-            write(fd[1], output, (strlen(output)+1));
-            wait(NULL);
-          }
+            pipe(fd);
+            close(fd[1]);
+            printf("Reading from pipe??\n");
+            read(fd[0], buffer, sizeof(buffer));
+            printf("Received string: %s at %d\n", buffer, getpid());
+            cpid = fork();
+            if(cpid < 0) {
+                printf("Error");
+                exit(1);
+            } else if (cpid == 0) { // child
+                printf("Child (%d): %d Parent: %d\n", 3, getpid(), getppid());
+                close(fd[1]);
+                printf("Reading from pipe??\n");
+                read(fd[0], buffer, sizeof(buffer));
+                printf("Received string: %s at %d\n", buffer, getpid());
+                exit(0);
+            } else  {
+              close(fd[0]);
+              printf("writing to pipe??\n");
+              write(fd[1], output, (strlen(output)+1));
+              wait(NULL);
+            }
 
-          exit(0);
-      } else  {
-        close(fd[0]);
-        printf("writing to pipe??\n");
-        write(fd[1], output, (strlen(output)+1));
-        wait(NULL);
-      }
+            exit(0);
+        } else  {
+          close(fd[0]);
+          printf("writing to pipe??\n");
+          write(fd[1], output, (strlen(output)+1));
+          wait(NULL);
+        }
+
+        exit(0);
+    } else  {
+      close(fd[0]);
+      printf("writing to pipe??\n");
+      write(fd[1], output, (strlen(output)+1));
+      wait(NULL);
+    }
 	return(0);
 }
 
