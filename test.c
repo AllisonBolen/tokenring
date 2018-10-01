ac0e86d2cd3ca2c77034f64d277ae6236c6abdcb
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-01 18:28:43 -0400

    testlevel

--- test.c
+++ test.c
@@ -15,16 +15,20 @@ int main(int argc, char* argv[])
     int fd[2];
     char buffer[256];
     char string[256];
-
+    pipe
 		// make fork
     // user userInput
     printf("What would you like your message to be: \n");
     fgets(string, sizeof(string), stdin);
-    //printf("Parent pid: %d\n\n", getpid());
+    printf("Parent pid: %d\n\n", getpid());
     char *pos;
     if ((pos=strchr(string, '\n')) != NULL)
       *pos = '\0';
 
+
+    for(int n = 0; n < 3; n++){
+
+    }
     pipe(fd);
     for(int i = 0; i < 3; i++){
     pid = fork();
@@ -32,14 +36,14 @@ int main(int argc, char* argv[])
         printf("Error");
         exit(1);
     } else if (pid == 0) { // child
-        printf("Child (%d): %d Parent: %d\n", 1, getpid(), getppid());
-        close(fd[1]);
-        read(fd[0], buffer, sizeof(buffer));
-        printf("%s\n", buffer);
+        printf("Child (%d): %d Parent: %d\n", n, getpid(), getppid());
+        // close(fd[1]);
+        // read(fd[0], buffer, sizeof(buffer));
+        //printf("%s\n", buffer);
     } else  {
-      close(fd[0]);
-      /* Send "string" through the output side of pipe */
-      write(fd[1], string, (strlen(string)+1));
+      // close(fd[0]);
+      // /* Send "string" through the output side of pipe */
+      // write(fd[1], string, (strlen(string)+1));
       wait(NULL);
     }
   }
