4fa24d04d02cb002c35532b0716d66a65d10261c
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-01 18:29:51 -0400

    testlevel

--- test.c
+++ test.c
@@ -30,22 +30,16 @@ int main(int argc, char* argv[])
 
     }
     pipe(fd);
-    for(int i = 0; i < 3; i++){
-    pid = fork();
-    if(pid < 0) {
-        printf("Error");
-        exit(1);
-    } else if (pid == 0) { // child
-        printf("Child (%d): %d Parent: %d\n", n, getpid(), getppid());
-        // close(fd[1]);
-        // read(fd[0], buffer, sizeof(buffer));
-        //printf("%s\n", buffer);
-    } else  {
-      // close(fd[0]);
-      // /* Send "string" through the output side of pipe */
-      // write(fd[1], string, (strlen(string)+1));
-      wait(NULL);
+    for(int i = 1; i < 3; i++){
+      pid = fork();
+      if(pid < 0) {
+          printf("Error");
+          exit(1);
+      } else if (pid == 0) { // child
+          printf("Child (%d): %d Parent: %d\n", i, getpid(), getppid());
+      } else  {
+        wait(NULL);
+      }
     }
-  }
 	return(0);
 }
