9ef6e0e36536f5aa50c0267755fcc7f6df90ff24
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-02 23:13:40 -0400

    this

--- test.c
+++ test.c
@@ -10,20 +10,43 @@
 #include <sys/types.h>
 // This is a c level program
 int *pidList;
+typedef struct {
+  char input[256];
+  int dst;
+} token;
 int main(int argc, char* argv[])
 {
 		int pid;
     int fd[2];
     // char buffer[256];
-		char buffer[256];
-    char string[256];
-		char numChildTemp[234];
+		token tok;
+    char numChildTemp[256];
+    int numChild = 0;
+    char destTemp[256];
+    signal(SIGINT, sigintHandler);
 
-		printf("How many machines would you like: \n");
+    printf("\nParent pid: %d\n\n", getpid());
+    printf("How many machines would you like: \n");
     fgets(numChildTemp, sizeof(numChildTemp), stdin);
-		int numChild = atoi(numChildTemp);
-		printf("What would you like your message to be: \n");
-    fgets(string, sizeof(string), stdin);
+
+		printf("At process: %d. What would you like your message to be: \n", getpid());
+		fgets(tok.input, sizeof(tok.input), stdin);
+		char *pos;
+		if ((pos=strchr(tok.input, '\n')) != NULL)
+			*pos = '\0';
+		printf("What would you like the destination of the message to be: \n");
+		fgets(destTemp, sizeof(destTemp), stdin);
+		printf("\n\n!!BEGIN!!\n\n");
+
+		tok.dst = atoi(destTemp);
+		if(tok.dst == 0){
+			exit(0);
+		}
+		numChild = atoi(numChildTemp);
+		if(tok.dst > numChild){
+			printf("\tThat machine doesnt exist!!!");
+			kill(getpid(), SIGINT);
+		}
 
 		pidList = (int*) malloc(numChild * sizeof(int));
 
@@ -47,9 +70,20 @@ int main(int argc, char* argv[])
 			pidList[i-1]= getpid();
 			printf("Child (%d): %d Parent: %d List at 0: %d.\n", i, getpid(), getppid(), pidList[0]);
 		}
-		// if(getpid == ){
-		//
-		// }
+		if(getpid() == pidList[0]){
+			write(fd[1], &tok, sizeof(token));
+		} else if( getpid() == pidList[tok.dst]) { // we are the destination process
+			read(fd[0], &tok, sizeof(token));
+			printf("\tReceived string: %s at %d.\n", tok.input, getpid());
+			tok.dst = 0;
+			strcpy(tok.input, "");
+			tok = tok;
+		} else if (getpid() == pidList[numChild]){ // tail of the list
+			printf("At the tail of the list");
+		} else{
+			read(fd[0], &tok, sizeof(token));
+			write(fd[1], &tok, sizeof(token));
+		}
 		wait(NULL);
 		printf("Ending: %d\n", getpid());
 		sleep(5);
